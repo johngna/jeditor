4474e6efd19d4cdc30964c943b7399dad0c88d7f
John <johngna@gmail.com>
2024-12-17 21:53:23 -0300

    correção namespaces

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,27 +4,27 @@ namespace Johngna\JEditor;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Livewire\Livewire;
-use J\Components\Editor\Editor;
+use Johngna\JEditor\Components\Editor\Editor;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/Components/Editor/resources/views', 'j-components');
+        $this->loadViewsFrom(__DIR__ . '/Components/Editor/resources/views', 'editor');
         
         $this->publishes([
-            __DIR__.'/config/j-components.php' => config_path('j-components.php'),
-        ], 'j-components-config');
+            __DIR__.'/config/j-editor.php' => config_path('j-editor.php'),
+        ], 'j-editor-config');
 
         $this->publishes([
-            __DIR__ . '/Components/Editor/resources/js' => public_path('vendor/j-components'),
-        ], 'j-components-assets');
+            __DIR__ . '/Components/Editor/resources/js' => public_path('vendor/j-editor'),
+        ], 'j-editor-assets');
 
         Livewire::component('j-editor', Editor::class);
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/config/j-components.php', 'j-components');
+        $this->mergeConfigFrom(__DIR__.'/config/j-editor.php', 'j-editor');
     }
 }
